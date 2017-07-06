a6f30eb74df206abb4c456e9244995b8aa72848a
Dimitrios J. Vergados <djvergad@gmail.com>
2017-07-06 16:18:14 +0200

    New run

--- runOnce.sh
+++ runOnce.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
 echo -n "$@ "
-~/local/bin/java -jar dist/Local_Voting.jar $@ | 
+. ~/.bash_profile
+java -jar dist/Local_Voting.jar $@ | 
     awk 'BEGIN{min=1e15}$1=="Connection:"&&!/NaN/{sum+=$2;sum2+=$2^2;count++;max=$2>max?$2:max;min=$2<min?$2:min}END{print sum/count,sum^2/(count*sum2),max,min,count}'
--- runParallel.sh
+++ runParallel.sh
@@ -4,4 +4,4 @@
 
 #parallel --gnu -k --sshloginfile <(~/scripts/build_sshlogin.sh) --workdir $PWD ./runonce.sh --time=50 --RngRun={1} --step={2} --x-size={3} --y-size={3} ::: {1..10} ::: 10 50 90 ::: {2..6} >> output
 rm output
-parallel --gnu --workdir $PWD ./runOnce.sh {1} {2} {3} {4} {5} {6} {7} ::: 100 ::: 10 ::: 100 ::: 10 ::: {30..1} ::: Simple LocalVoting LQF DRAND Lyui Lobats LQFLinkScheduller ::: {1..5} >> output
+parallel --gnu --sshloginfile <(~/scripts/build_sshlogin.sh) --workdir $PWD ./runOnce.sh {1} {2} {3} {4} {5} {6} {7} ::: 100 ::: 10 ::: 100 ::: 10 ::: {30..1} ::: Simple LocalVoting LQF DRAND Lyui Lobats LQFLinkScheduller ::: {1..500} >> output
